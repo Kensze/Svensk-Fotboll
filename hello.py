c0d244a7ebf6afa3288f20ad73bb04fdc83b10e2
KenSze <kennetsze@gmail.com>
2015-10-01 12:08:00 +0000

    Json exempel

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from bottle import route, run, template
+import json
 
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='localhost', port=8080)
+@route('/')
+def returnsingle():
+        return { "id": 1, "name": "Test Item 1" }
+
+run(host='localhost', port=8080, debug=True, reloader=True)
