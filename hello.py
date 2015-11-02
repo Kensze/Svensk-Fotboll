b9e4e80f2e59ba83f9833677d27cd41a5774eb61
Hagerstrom <danielhagerstrom@gmail.com>
2015-11-02 12:22:19 +0100

    Update

--- hello.py
+++ hello.py
@@ -5,25 +5,10 @@ from flask import Flask, render_template, jsonify, request
 from flask.ext.cors import CORS, cross_origin
 from flask.ext.triangle import Triangle
 
-
-#from flask.ext import restful
-#from flask.ext.restful import Api
-
-
 app = Flask(__name__, static_path='/static')
 cors = CORS(app, allow_headers='Content-Type', CORS_SEND_WILDCARD=True)
 app.config['CORS_HEADERS'] = 'Content-Type'
 Triangle(app)
-#api = restful.Api(app)
-#app = Flask(__name__)
-app.debug = True
-
-"""@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response"""
 
 
 
@@ -42,7 +27,6 @@ def movie(ID):
 def trailers(id):
         parameters = {'imdb' : id, 'count' : 1, 'format' : 'json' }
         response = urllib2.urlopen('http://www.myapifilms.com/taapi?' + urllib.urlencode(parameters))
-	print 'http://www.omdbapi.com/?' + urllib.urlencode(parameters)
         movies = json.loads(response.read())
         if not movies:
             return jsonify({"Error": "Incorrect IMDb ID", "Response": "False"})
@@ -57,7 +41,6 @@ def search():
 	query = post.get('search')
 	parameters = {'s' : query, 'r' : 'json'}
 	response = urllib2.urlopen('http://www.omdbapi.com/?' + urllib.urlencode(parameters))
-	#print 'http://www.omdbapi.com/?' + urllib.urlencode(parameters)
 	movies = json.loads(response.read())
 	return jsonify(movies)
 
@@ -66,27 +49,8 @@ def search():
 def view(id):
     	parameters = {'i' : id, 'plot' : 'short', 'r' : 'json'}
 	response = urllib2.urlopen('http://www.omdbapi.com/?' + urllib.urlencode(parameters))
-	#print 'http://www.omdbapi.com/?' + urllib.urlencode(parameters)
 	movies = json.loads(response.read())
 	return jsonify(movies)
-
-
-@app.route('/trailer', methods=['POST', 'GET', 'OPTIONS'])
-@cross_origin(origins='*', send_wildcard=True)
-def trailer():
-    	post = request.get_json()
-	query = post.get('search')
-	parameters = {'movie' : query, 'limit' : 5, 'width' : 320}
-	tmp = 'http://trailersapi.com/trailers.json?' + urllib.urlencode(parameters)
-	response = urllib2.urlopen('http://trailersapi.com/trailers.json?' + urllib.urlencode(parameters))
-	#print tmp
-	movies = json.loads(response.read())
-	#print movies
-	#print json.dumps(movies)
-	return json.dumps(movies)
-
-
-
 
 
 if __name__ == "__main__":
--- static/js/hello.js
+++ static/js/hello.js
@@ -8,58 +8,28 @@ var app = angular.module('myApp', ['ngSanitize'])
 
 app.controller('myController', ['$scope', '$http','$location', '$window', '$sce',  function($scope, $http, $location, $window, $sce, $filter, $compile) {
 
-    $scope.$sce = $sce;
-
-    $scope.to_trusted = function(html_code) {
-        return $sce.trustAsHtml(html_code);
-    }
-
-
-    //app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
-
 
     $scope.imdbID = function() {
-        //$scope.ID = $location.pathname();
-        //$scope.ID = $window.location.pathname;
-
         var pathname = $window.location.pathname.substring(1);
         var parts = pathname.split(/\//);
         if (parts.length > 1 ) {
             $scope.ID = parts[1];
         }
-        console.log($scope.ID);
-
-        //$scope.ID = "asd";
         $http.get('/movies/' + $scope.ID).success(function(data) {
             $scope.greeting = data;
     });
     }
 
-
-
     $scope.imdbTrailer = function() {
-        //$scope.ID = $location.pathname();
-        //$scope.ID = $window.location.pathname;
-
         var pathname = $window.location.pathname.substring(1);
         var parts = pathname.split(/\//);
         if (parts.length > 1 ) {
             $scope.ID = parts[1];
         }
-        console.log($scope.ID);
-
-        //$scope.ID = "asd";
-       //$http.get('http://www.myapifilms.com/taapi?imdb=' + $scope.ID + '&count=1&format=JSON').success(function(data) {
        $http.get('/trailers/' + $scope.ID).success(function(data) {
-            //var trailer1 = JSON.parse(data);
-            $scope.trailer = data;
-        console.log($scope.trailer);
-        console.log($scope.trailer.trailer);
-        console.log($scope.trailer.embed);
+        $scope.trailer = data;
     });
     }
-
-
 
     $scope.clickButton = function() {
         $http.get('/update').success(function(data) {
@@ -76,17 +46,5 @@ app.controller('myController', ['$scope', '$http','$location', '$window', '$sce'
             $scope.greeting = data;
         });
     }
-
-    $scope.doTrailer = function(query){
-        $http({
-            url: 'trailer',
-            method: "POST",
-            data: {search: query},
-        }).success(function(data) {
-            $scope.trailers = data;
-        });
-    }
-
-
 
     }]);
